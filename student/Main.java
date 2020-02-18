cfbf4aaf560a39cec2405d38931d624bdad88053
Toby Vincent <tobyv13@gmail.com>
2020-02-18 13:56:17 -0600

    v2.0 - Array of objects

--- student/Main.java
+++ student/Main.java
@@ -1,88 +1,26 @@
-
 /**
  * Main
  */
-import java.util.Scanner;
-import java.io.File;
-import java.io.PrintWriter;
-import java.io.FileNotFoundException;
-
 public class Main {
-    private static final int MAX_STUDENT_COUNT = 10;
-    private static Student[] students = new Student[MAX_STUDENT_COUNT];
-    private static int length = 0;
-    private static final int SID = 0;
-    private static final int FIRST = 1;
-    private static final int LAST = 2;
-    private static final int ACADEMICYEAR = 3;
-    private static final int MAJOR = 4;
 
     public static void main(String[] args) {
-        try {
-            int length = loadStudentsFromFile("students.txt");
-            for (int i = 0; i < length; i++) {
-                System.out.printf("%s%n", students[i]);
-            }
-            writeStudentsToFile("studentsOut.txt");
+        Student[] students = { new Undergraduate(800, "John", "Doe", "CS", "Fr"),
+                new Graduate(801, "Jane", "Doe", "CS", "Someone"), new Undergraduate(802, "Paul", "Doe", "CS", "Jr") };
 
-        } catch (FileNotFoundException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
+        for (Student student : students) {
+            System.out.println(student);
         }
 
-        
-        // Add some courses to the first student record.
-        students[0].addCourse("CS140","Introduction to Computing I", 4.0, "A");
-        students[0].addCourse("CS150","Introduction to Computing II", 3.0, "B");
-        
-        // Display the first student's trascript.
-        System.out.printf("%n%s", students[0].transcript());
-        System.out.println("\nProgram ended sucessfully.");
-    }
+        Student o = students[0];
 
-    private static int loadStudentsFromFile(String filename) throws FileNotFoundException {
-        // try w/ resources | closes resources
-        try (Scanner scanner = new Scanner(new File(filename));) {
-            int sid = 0;
-            String first = null;
-            String last = null;
-            String academicYear = null;
-            String major = null;
-
-            String record;
-            String[] fields;
-
-            int length = 0;
-
-            while (scanner.hasNextLine()) {
-
-                record = scanner.nextLine();
-                fields = record.split(",");
-
-                // stored as string in file but needs parsed as int
-                sid = Integer.parseInt(fields[SID]);
-                first = fields[FIRST];
-                last = fields[LAST];
-                academicYear = fields[ACADEMICYEAR];
-                major = fields[MAJOR];
-
-                // assign to current index and incriment
-                students[length++] = new Student(sid, first, last, academicYear, major);
-            }
-            return length;
-        }
-
-    }
-    private static void writeStudentsToFile(String filename) throws FileNotFoundException {
-        try ( PrintWriter pw = new PrintWriter( new File(filename) ); ) {
-            for (int index = 0; index < length; index++) {
-                //pw.println(students[index].getSid());
-                pw.printf("%d,", students[index].getSid());
-                pw.printf("%s,", students[index].getFirst());
-                pw.printf("%s,", students[index].getLast());
-                pw.printf("%s,", students[index].getAcademicYear());
-                pw.printf("%s%n", students[index].getMajor());
-            }
+        if (o instanceof Undergraduate) {
+            // Undergraduate object
+            Undergraduate u = (Undergraduate) o;
+            System.out.println(u.getAcademicYear());
+        } else {
+            // Graduate object
+            Graduate g = (Graduate) o;
+            System.out.println(g.getAcademicAdvisor());
         }
     }
 }
--- student/Student.java
+++ student/Student.java
@@ -2,117 +2,61 @@
  * Student
  */
 public class Student {
-
-        private class Course {
-        public String courseNumber;
-        public String title;
-        public double creditHours;
-        public String grade;
-    
-        public Course() {
-            this("CS100", "Fictitious course", 3.0, "A");
-        }// end Course()
-    
-        public Course(String courseNumber, String title, double creditHours, String grade) {
-            this.courseNumber = courseNumber;
-            this.title = title;
-            this.creditHours = creditHours;
-            this.grade = grade;
-        }
-           
-        @Override
-        public String toString() {
-            String str = "";
-            
-            str += String.format("%s-%s, %2.1f, %s", courseNumber, title, creditHours, grade);
-            
-            return str;
-        }// end toString()
-    
-    }// end Course
-
     private int sid;
     private String first;
     private String last;
-    private String academicYear;
     private String major;
 
     public Student() {
-        this(0, null, null, null, null);
+        this(0, null, null, null);
     }
 
-    public Student(int sid, String first, String last, String academicYear, String major) {
+    public Student(int sid, String first, String last, String major) {
         setSid(sid);
         setFirst(first);
         setLast(last);
-        setAcademicYear(academicYear);
         setMajor(major);
     }
 
-    public int getSid() {
+    public final int getSid() {
         return sid;
     }
 
-    public String getFirst() {
+    public final void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public final String getFirst() {
         return first;
     }
 
-    public String getLast() {
-        return last;
-    }
-    
-    public String getAcademicYear() {
-        return academicYear;
+    public final void setFirst(String first) {
+        this.first = first;
     }
 
-    public String getMajor() {
+    public final String getLast() {
+        return last;
+    }
+
+    public final void setLast(String last) {
+        this.last = last;
+    }
+
+    public final String getMajor() {
         return major;
     }
 
-    public void setSid(int sid) {
-        this.sid = sid;
-    }                
-    
-    public void setFirst(String first) {
-        this.first = first;
-    }            
-
-    public void setLast(String last) {
-        this.last = last;
-    }        
-
-    public void setAcademicYear(String academicYear) {
-        this.academicYear = academicYear;
-    }    
-
-    public void setMajor(String major) {
+    public final void setMajor(String major) {
         this.major = major;
-    }
-        
-    public void addCourse(String courseNumber, String title, double creditHours, String grade) {
-        courses[courseCount++] = new Course(courseNumber, title, creditHours, grade);
-    }
-    
-    public String transcript() {
-        String str = String.format("Name: %s, %s(%s)", last, first, academicYear);
-        str += String.format(", Major: %s", major);
-        
-        for (int index = 0; index < courseCount; index++) {
-            str += String.format("%n%4s%s", " ", courses[index]);
-        }
-        return str;
     }
 
     @Override
     public String toString() {
         String str = "";
 
-        str += String.format("[%d]", sid);
-        str += String.format("[%s, %s]", last, first);
-        str += String.format("[%s]", academicYear);
-        str += String.format("[%s]", major);
+        str += String.format("[%d][%s, %s][%s]", sid, last, first, major);
 
         return str;
     }
-
+    
 }
