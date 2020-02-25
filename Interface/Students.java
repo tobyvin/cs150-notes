9ee7c245b7fbd855a91c36b315f25965d75bc0a7
Toby Vincent <tobyv13@gmail.com>
2020-02-25 14:16:49 -0600

    Interfaces - half done, broken

--- Interface/Students.java
+++ Interface/Students.java
@@ -2,6 +2,28 @@
  * Students
  */
 public class Students implements Sortable {
+    Student[] roster = null;
 
+
+    @Override
+    public void sort() {
+        for (int top = 0; top < roster.length; top++) {
+            swap(top, indexOfSmallestElement(top));
+        }
+    }
     
+    @Override
+    private int indexOfSmallestElement(int Startup) {
+        
+    }
+
+    @Override
+    public String toString() {
+
+        String str = "";
+        for (Student student : roster) {
+            str += 
+        }
+        return String.format("Students []");
+    }
 }
