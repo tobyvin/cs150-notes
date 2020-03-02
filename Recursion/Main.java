bd3231cd3ee86c9f3793f527dadd4d4d4d780626
Toby Vincent <tobyv13@gmail.com>
2020-03-02 13:58:24 -0600

    recursion

---
+++ Recursion/Main.java
@@ -0,0 +1,31 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // System.out.printf("%.0f", fact(5));
+        System.out.printf("%.0f", fib(10));
+
+    }
+    
+    private static double fact(int n) {
+        if (n == 0) {
+            // Base case
+            return 1;
+        } else {
+            // Recursive case
+            return n * fact(n - 1);
+        }
+    }
+    
+    private static double fib(int n) {
+        // Fibanachi
+        // N_n = N_n-1 + N_n-2
+        if (n == 1 || n == 2 ) {
+            return n;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+}
